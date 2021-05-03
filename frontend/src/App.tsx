e528f8d7ae297fbd677857c2f8f0fbd957fef408
andrezasecon <andrezasecon@gmail.com>
2021-05-03 17:50:25 -0300

    Add Statics charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/footer";
 import NavBar from "components/NavBar";
 
@@ -8,7 +10,21 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo</h1>
+        <h1 className= "text-primary" py-3>DashBoard de vendas</h1>
+        <div className="row px-3">
+          <div className = "col-sm-6">
+            <h5 className ="text-center text-secundary"> Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+
+          <div className = "col-sm-6">
+            <h5 className = "text-center text-secundary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">Todas as Vendas</div>
+        
         <DataTable />
       </div>
       <Footer />
